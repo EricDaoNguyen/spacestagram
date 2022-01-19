311575c8961f055153e8f284d250c649b79a7106
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2022-01-19 17:10:01 -0500

    Feat: Linked icons to information page.

--- src/components/InformationPage.js
+++ src/components/InformationPage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from './NavBar';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FolderIcon from '@mui/icons-material/Folder';
+import Link from '@material-ui/core/Link';
 
 export default function InformationPage() {
   return (
     <>
       <NavBar />
       <div className="information-page">
-        <h1>Welcome to the information page!</h1>
+        <Link href="https://github.com/EricDaoNguyen/spacestagram" target="_blank"><FolderIcon /></Link>
+        <Link href="https://www.linkedin.com/in/ericdaonguyen/" target="_blank"><LinkedInIcon /></Link>
+        <Link href="https://github.com/EricDaoNguyen" target="_blank"><GitHubIcon /></Link>
       </div>
     </>
   )
