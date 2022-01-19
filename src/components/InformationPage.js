5a76a09f8dfd4272621cc31a83c5b00186a616a3
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2022-01-19 17:14:41 -0500

    Feat: Added description to information page.

--- src/components/InformationPage.js
+++ src/components/InformationPage.js
@@ -9,10 +9,21 @@ export default function InformationPage() {
   return (
     <>
       <NavBar />
-      <div className="information-page">
+      <div className="information-page-icons">
         <Link href="https://github.com/EricDaoNguyen/spacestagram" target="_blank"><FolderIcon /></Link>
         <Link href="https://www.linkedin.com/in/ericdaonguyen/" target="_blank"><LinkedInIcon /></Link>
         <Link href="https://github.com/EricDaoNguyen" target="_blank"><GitHubIcon /></Link>
+      </div>
+      <div className="information-page-text">
+        <p>
+          After serving the Marine Corps from 2013 to 2017 and training my clients for a couple of years as a personal trainer afterwards, I decided to make a career change and follow my passion for computers and start creating projects with code. Jumping into software development and attending at Fullstack Academy was the best decision I've ever made as I love creating new applications while being in the process of learning something new each day! As of now, I'm a recent graduate and also a teaching fellow of Fullstack Academy.
+        </p>
+        <p>
+          Proficient: JavaScript, HTML, CSS, Git, Github, Node/Express, React, React Native, Redux, PostgreSQL, Sequelize, Firebase, and Cloud Firestore.
+        </p>
+        <p>
+          Some Familiarity: Java, Python.
+        </p>
       </div>
     </>
   )
